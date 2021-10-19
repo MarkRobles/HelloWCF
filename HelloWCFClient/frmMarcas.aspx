c98b8bab2f0639b152850c52a9166d00a24ec78a
Marcos Robles <marcos.robles@dalton.com.mx>
2021-10-19 12:28:45 -0500

    # Agregar marcas - backEnd

--- HelloWCFClient/frmMarcas.aspx
+++ HelloWCFClient/frmMarcas.aspx
@@ -1,9 +1,25 @@
 ﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="frmMarcas.aspx.vb" Inherits="HelloWCFClient.frmMarcas" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-
-
     <asp:GridView ID="grdMarcas" runat="server"></asp:GridView>
+    <asp:Button ID="btnAgregar" runat="server" Text="Agregar" OnClick="btnAgregar_Click" Width="200px"  />
 
-   <asp:TextBox ID="txtDescripcionCorta" runat="server"  Text="DescripcionCorta" PlaceHolder="Descripción Corta" ></asp:TextBox>
+    <asp:Panel ID="popDatos" runat="server" Visible="false" ScrollBars="Both">
+        <asp:Label ID="lblDescripcionCorta" runat="server" Text="Descripción Corta"></asp:Label>
+        <asp:TextBox ID="txtDescripcionCorta" runat="server" Text="DescripcionCorta" PlaceHolder="Descripción Corta"></asp:TextBox>
 
+        <asp:Label ID="lblDescripcionLarga" runat="server" Text="Descripción Larga"></asp:Label>
+        <asp:TextBox ID="txtDescripcionLarga" runat="server" Text="DescripcionLarga" PlaceHolder="Descripción Larga"></asp:TextBox>
+
+        <asp:Label ID="lblMargen" runat="server" Text="Margen"></asp:Label>
+        <asp:TextBox ID="txtMargen" runat="server" Text="Margen" PlaceHolder="Margen"></asp:TextBox>
+
+        <asp:Label ID="lblVisibilidad" runat="server" Text="¿Precio Visible al publico?"></asp:Label>
+        <asp:CheckBox ID="chkVisibilidad" runat="server" />
+
+        <asp:Label ID="lblSlogan" runat="server" Text="Slogan"></asp:Label>
+        <asp:TextBox ID="txtSlogan" runat="server" Text="Slogan" PlaceHolder="Slogan"></asp:TextBox>
+
+        <asp:Button ID="btnGuardar" runat="server" Text="Guardar"  OnClick="btnGuardar_Click" />
+    </asp:Panel>
 </asp:Content>
--- HelloWCFClient/frmMarcas.aspx.vb
+++ HelloWCFClient/frmMarcas.aspx.vb
@@ -1,14 +1,21 @@
-﻿Public Class frmMarcas
+﻿Imports Entities
+
+Public Class frmMarcas
     Inherits System.Web.UI.Page
 
+
+
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        ObtenerMarcas()
+    End Sub
+
+
+    Sub ObtenerMarcas()
         Dim proxy As New AliveIdeaService.AliveIdeaServiceClient("NetTcpBinding_IAliveIdeaService")
         'proxy.ClientCredentials.Windows.ClientCredential.UserName = "DOMAIN\\User"
         'proxy.ClientCredentials.Windows.ClientCredential.Password = "Password"
 
         Try
-
-
             Dim marcas = proxy.ObtenerMarcas()
             grdMarcas.DataSource = marcas
             grdMarcas.DataBind()
@@ -18,7 +25,34 @@
         Finally
             proxy.Close() 'Liberar los recursos utilizados por el proxy aunque ocurra un error
         End Try
-
     End Sub
 
+    Protected Sub btnAgregar_Click(sender As Object, e As EventArgs)
+        popDatos.Visible = True
+    End Sub
+
+    Protected Sub btnGuardar_Click(sender As Object, e As EventArgs)
+        Dim proxy As New AliveIdeaService.AliveIdeaServiceClient("NetTcpBinding_IAliveIdeaService")
+        Try
+            Dim marca As New Marca
+            marca.DescripcionCorta = txtDescripcionCorta.Text.Trim
+            marca.DescripcionLarga = txtDescripcionLarga.Text.Trim
+            marca.VisibilidadPrecio = chkVisibilidad.Checked
+            marca.Margen = txtMargen.Text.Trim
+            marca.Slogan = txtSlogan.Text.Trim
+
+            Dim resultado = proxy.CrearMarca(marca)
+
+            If resultado >= 1 Then
+                clsComunicacionConFrontEnd.MostrarSweetAlert(Me, "Marca registrada correctamente")
+            End If
+
+
+        Catch ex As Exception
+            MostrarSweetAlert(Me, ex.Message)
+        Finally
+            proxy.Close() 'Liberar los recursos utilizados por el proxy aunque ocurra un error
+            ObtenerMarcas()
+        End Try
+    End Sub
 End Class
