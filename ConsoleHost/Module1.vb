15157fd43e7296acbab1a0607359423b3660b56b
Marcos Robles <marcos92robles@gmail.com>
2021-10-20 22:43:50 -0500

    # Agregar endpoints dinamicamente en codigo en selfhost, sin configuracion en webcofig o appconfig

--- ConsoleHost/Module1.vb
+++ ConsoleHost/Module1.vb
@@ -1,10 +1,19 @@
 ﻿Imports System.ServiceModel
+Imports System.ServiceModel.Channels
 Imports HelloWCF
 
 Module Module1
 
     Sub Main()
         Dim hostHelloWCF As New ServiceHost(GetType(AliveIdeaService))
+        'Para agregar endpoints de forma dinamica se debe hacer antes de abrir el host
+        Dim address As String = "net.tcp://localhost:8009/AliveIdeaService"
+        'Cada binding tiene una clase propia
+        Dim binding As Binding = New NetTcpBinding
+        Dim contract As Type = GetType(IAliveIdeaService)
+
+        hostHelloWCF.AddServiceEndpoint(contract, binding, address)
+
         hostHelloWCF.Open()
         Console.WriteLine("Servicio iniciado. Presiona [Enter] para salir.")
         Console.ReadLine()
